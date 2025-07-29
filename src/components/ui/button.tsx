4e642b8d367b4fd41dcd9d9d552dbd8aa55a3416
Johsam <johsamfcress@gmail.com>
2025-07-29 19:53:34 +0200

    updated danger variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -22,7 +22,7 @@ const Button: React.FC<ButtonProps> = ({
   const variants = {
     primary: "rounded-lg flex items-center gap-1 text-gray-400 hover:text-blue-400",
     secondary: "rounded-lg px-4 py-2 flex w-full items-center justify-center gap-2 rounded-lg bg-white py-2 text-black shadow-md transition hover:bg-gray-300 cursor-pointer active:opacity-90",
-    danger: "bg-red-600 text-white hover:bg-red-700",
+    danger: "mt-2 mr-2 p-2 bg-red-600 hover:bg-red-700 text-white rounded-full",
     outline: "border border-gray-400 text-gray-800 hover:bg-gray-100",
     send:"mt-2 mr-2 p-2 bg-blue-600 hover:bg-blue-700 text-white rounded-full flex items-center justify-center self-end"
   };
