a3a258f67539cedcce1a200188d27e1fba9c3bae
Johsam <johsamfcress@gmail.com>
2025-07-29 15:38:56 +0200

    added send variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,7 +4,7 @@ type ButtonProps = {
   children: React.ReactNode;
   onClick?: () => void;
   type?: "button" | "submit" | "reset";
-  variant?: "primary" | "secondary" | "danger" | "outline";
+  variant?: "primary" | "secondary" | "danger" | "outline" | "send";
   disabled?: boolean;
   className?: string;
 };
@@ -18,12 +18,13 @@ const Button: React.FC<ButtonProps> = ({
   className = "",
 }) => {
   const baseStyles =
-    "rounded-lg text-sm font-medium transition duration-200 ease-in-out cursor-pointer";
+    "text-sm font-medium transition duration-200 ease-in-out cursor-pointer";
   const variants = {
-    primary: "flex items-center gap-1 text-gray-400 hover:text-blue-400",
-    secondary: "px-4 py-2 flex w-full items-center justify-center gap-2 rounded-lg bg-white py-2 text-black shadow-md transition hover:bg-gray-300 cursor-pointer active:opacity-90",
+    primary: "rounded-lg flex items-center gap-1 text-gray-400 hover:text-blue-400",
+    secondary: "rounded-lg px-4 py-2 flex w-full items-center justify-center gap-2 rounded-lg bg-white py-2 text-black shadow-md transition hover:bg-gray-300 cursor-pointer active:opacity-90",
     danger: "bg-red-600 text-white hover:bg-red-700",
     outline: "border border-gray-400 text-gray-800 hover:bg-gray-100",
+    send:"mt-2 mr-2 p-2 bg-blue-600 hover:bg-blue-700 text-white rounded-full flex items-center justify-center self-end"
   };
 
   return (
