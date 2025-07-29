f12a0f711ac0a277934a4da17f704a2895dfc44c
Johsam <johsamfcress@gmail.com>
2025-07-29 20:17:49 +0200

    added feature functionality

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router-dom';
 import HeaderProfile from '@/components/headerProfile';
 import Button from '@/components/ui/button';
 import Comments from '@/components/comments';
+import { FaUserCircle } from 'react-icons/fa';
+import { LogOut } from 'lucide-react';
 
 interface Post {
   id: string;
@@ -14,24 +16,40 @@ interface Post {
     likes: number;
     comments?: number; 
   };
+  author:{
+    name: string;
+  }
   createdAt: string;
 }
+
+type Like = {
+    id: string;
+    postId: string;
+    userId: string;
+    createdAt: string;
+  };
+  
 
 function Dashboard() {
     const [posts, setPosts] = useState<Post[]>([]);
     const [loading, setLoading] = useState(true);
     const [likedPosts, setLikedPosts] = useState<Set<string>>(new Set());
     const [errorMsg, setErrorMsg] = useState("");  
+    const [displayComments, setDisplayComments] = useState<string>("");
     const navigate = useNavigate();
 
     const API_BASE_URL = "http://localhost:3000";
 
     async function sessionHandler() {
-        const { data: session} = await authClient.getSession();
-        if (!session?.user) {
-        navigate("/login")
+        try {
+            const { data: session } = await authClient.getSession();
+            if (!session?.user) navigate("/login");
+        } catch (err) {
+            console.error("Session check failed", err);
+            navigate("/login");
         }
     }
+    
 
     const fetchPosts = async () => {
         try {
@@ -40,7 +58,6 @@ function Dashboard() {
         });
         const data = await res.json();
         setPosts(data);
-        console.log(data)
         setLoading(false);
         } catch (err) {
         console.error("Failed to fetch posts:", err);
@@ -48,22 +65,30 @@ function Dashboard() {
         }
     };
 
-    const fetchPostLikes = async () => {
-        try {
-            const res = await fetch(`${API_BASE_URL}/api/posts/:id/likes`, {
-                credentials: "include"
-            });
-            const likes = await res.json();
-            console.log(likes);
-        } catch (error) {
-            console.error("Failed to fetch likes:", error)
-        }
-    }
-
     useEffect(() => {
+        let isMounted = true;
         sessionHandler();
         fetchPosts();
+        getUserLikes();
+        return () => { isMounted = false };
     }, []);
+    
+
+    const getUserLikes = async () => {
+        try {
+            const res = await fetch(`${API_BASE_URL}/api/likes`, {
+                credentials: "include"
+            });
+            const likes: Like[] = await res.json();
+            const likedPostIds = new Set<string>(likes.map(like => like.postId));
+
+            setLikedPosts(likedPostIds);
+
+        } catch (error) {
+            console.error("Failed to get user likes:", error);
+        }
+    };
+    
 
     const toggleLike = async (postId: string) => {
         setErrorMsg("");
@@ -98,18 +123,42 @@ function Dashboard() {
         } 
     };
 
-    const displayComments = async (postId: string) => {
+    const commentsPanel = async (postId: string) => {
+        if(displayComments == postId) return setDisplayComments("")
+        setDisplayComments(postId);
         setErrorMsg("");
+    }
+
+
+    const incrementCommentCount = (postId: string) => {
+        setPosts(prev =>
+            prev.map(post =>
+            post.id === postId
+                ? {
+                    ...post,
+                    _count: {
+                    ...post._count,
+                    comments: (post._count.comments ?? 0) + 1,
+                    },
+                }
+                : post
+                //?? is the nullish coalescing operator.
+                //If post._count.comments is undefined or null, it falls back to 0.
+            )
+        );
+    };
+
+    const signOut = async () => {
         try {
-            const res = await fetch(`${API_BASE_URL}/api/posts/${postId}/comments`, {
-                credentials: "include"
-            });
-            const commets = await res.json();
+            const res = await authClient.signOut();
+            if(res.data?.success){
+                navigate("/login");
+            }
         } catch (error) {
-            console.error(error);
-            setErrorMsg("failed to load comments");
+            console.error("Sign out error:", error)
         }
     }
+
 
     if (loading) return <div className="p-8 text-center text-lg">Loading posts...</div>;
 
@@ -117,9 +166,16 @@ function Dashboard() {
         <div className="min-h-screen text-white px-4 py-6">
         
             {/* Header */}
-            <header className="flex items-center justify-between mb-10">
-                <div className="w-12 h-12 rounded-full bg-white/10" />
-                <h1 className="text-xl sm:text-2xl font-bold text-center flex-1 -ml-12">
+            
+            <header className="flex items-center justify-between mb-2">
+            <Button
+                onClick={signOut}
+                variant='danger'
+            >
+                <LogOut className="p-1" />
+            </Button>
+
+                <h1 className="text-xl sm:text-2xl font-bold text-center -ml-12">
                     Find All Blogs
                 </h1>
                 <HeaderProfile />
@@ -132,74 +188,87 @@ function Dashboard() {
             {errorMsg && (
                 <p className="mb-4 p-1 rounded-sm text-center bg-red-800">{errorMsg}</p>
             )}
-
-            {/* spinner */}
-            {loading && <span className="animate-spin mr-2 h-4 w-4 border-2 border-black border-t-transparent rounded-full"></span>}
             {/* Posts */}
             <div className="flex flex-col gap-6 max-w-2xl mx-auto">
+                {!loading && posts.length === 0 && (
+                    <p className="text-center text-gray-400">No posts yet.</p>
+                )}
+                {/* spinner */}
+                {loading && <span className="animate-spin mr-2 h-4 w-4 border-2 border-black border-t-transparent rounded-full"></span>}
+            
                 {posts.map( (post:any) => (
-                    <div
-                        key={post.id}
-                        className="bg-[#1a1a1a] p-6 rounded-2xl shadow-md hover:shadow-lg transition"
-                    >
-                        <div className='flex justify-between'>
+                    <section key={post.id}>
+                        <div
+                            className="bg-[#1a1a1a] p-6 rounded-2xl shadow-md hover:shadow-lg transition"
+                        >
+                            <div className='flex justify-between'>
+                                <h2 className="text-sm font-semibold mb-2 capitalize text-gray-300">
+                                    {<FaUserCircle className="w-5 h-5 text-white/60 mr-1 inline" />} {post.author.name}
+                                </h2>
+                                <span className='text-sm text-gray-500 mb-2'>{new Date(post.createdAt).toLocaleDateString()}</span>
+                            </div>
                             <h2 className="text-xl font-semibold mb-2 ">{post.title}</h2>
-                            <span className='text-sm text-gray-500 mb-2'>{new Date(post.createdAt).toLocaleDateString()}</span>
-                        </div>
-                        <p className="text-gray-400 text-sm mb-4">
-                        {post.content}
-                        </p>
+                            <p className="text-gray-400 text-sm mb-4">
+                            {post.content}
+                            </p>
 
-                        <div className="flex items-center justify-between text-sm text-gray-500">
-                            <div className="flex gap-4">
-                                <Button
-                                onClick={() => toggleLike(post.id)}
-                                className={`${
-                                    likedPosts.has(post.id)
-                                    ? "text-red-500"
-                                    : "text-gray-400"
-                                } hover:text-red-300`}
-                                >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill={likedPosts.has(post.id) ? "currentColor" : "none"}
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                    className="w-5 h-5"
-                                >
-                                    <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={1.5}
-                                    d="M4.318 6.318a4.5 4.5 0 016.364 0L12 7.636l1.318-1.318a4.5 4.5 0 116.364 6.364L12 20.364l-7.682-7.682a4.5 4.5 0 010-6.364z"
-                                    />
-                                </svg>
-                                {post._count.likes}
-                                </Button>
-
-                                <Button
-                                    onClick={() => {displayComments(post.id)}}
-                                >
+                            <div className="flex items-center justify-between text-sm text-gray-500">
+                                <div className="flex gap-4">
+                                    <Button
+                                    onClick={() => toggleLike(post.id)}
+                                    className={`${
+                                        likedPosts.has(post.id)
+                                        ? "text-red-500"
+                                        : "text-gray-400"
+                                    } hover:text-red-300`}
+                                    >
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
+                                        fill={likedPosts.has(post.id) ? "currentColor" : "none"}
                                         viewBox="0 0 24 24"
                                         stroke="currentColor"
                                         className="w-5 h-5"
                                     >
-                                    <path
+                                        <path
                                         strokeLinecap="round"
                                         strokeLinejoin="round"
                                         strokeWidth={1.5}
-                                        d="M8 10h.01M12 10h.01M16 10h.01M21 12c0 4.418-4.03 8-9 8-1.49 0-2.89-.326-4.083-.9L3 20l1.23-3.692C3.451 14.743 3 13.405 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                                        d="M4.318 6.318a4.5 4.5 0 016.364 0L12 7.636l1.318-1.318a4.5 4.5 0 116.364 6.364L12 20.364l-7.682-7.682a4.5 4.5 0 010-6.364z"
                                         />
                                     </svg>
-                                    {post._count.comments}
-                                </Button>
+                                    {post._count.likes}
+                                    </Button>
+
+                                    <Button
+                                        onClick={() => {commentsPanel(post.id)}}
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                            className="w-5 h-5"
+                                        >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth={1.5}
+                                            d="M8 10h.01M12 10h.01M16 10h.01M21 12c0 4.418-4.03 8-9 8-1.49 0-2.89-.326-4.083-.9L3 20l1.23-3.692C3.451 14.743 3 13.405 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                                            />
+                                        </svg>
+                                        {post._count.comments}
+                                    </Button>
+                                </div>
                             </div>
+                            
                         </div>
-                        <Comments/>
-                    </div>
+                        {displayComments == post.id && 
+                        <Comments 
+                            API_BASE_URL={API_BASE_URL} 
+                            postId={post.id}
+                            onNewComment={() => incrementCommentCount(post.id)}
+                        />}
+                    </section>
                 ))}
             </div>
         </div>
